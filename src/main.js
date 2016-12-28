073c45a3e247d0386e340385f7d2d0740b772917
Ivan Galarza <nadeyyo@gmail.com>
2016-12-29 00:25:57 +0100

    Added exports to main.js and some minor fixes

--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@
 // Here we import all "public" classes to expose them 
 import { SimpleLayout } from 'layout/SimpleLayout';
 import { TwoColumnsLayout } from 'layout/TwoColumnsLayout';
+
+export { SimpleLayout, TwoColumnsLayout };
