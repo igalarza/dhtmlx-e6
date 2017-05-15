2384b97c92a38a5262305d24e0f5a81fc30dcc46
Ivan Galarza <nadeyyo@gmail.com>
2017-05-15 10:09:16 +0200

    Messages improved, now all return promises.

--- src/window/Message.js
+++ src/window/Message.js
@@ -3,71 +3,88 @@
 export class Message {
 
 	static alert (title, text, modal = false) {
-
-		if (modal) {
-			dhtmlx.message({
-			    title: title,
-			    type: 'alert',
-			    text: text
-			});
-		} else {
-			dhtmlx.alert({
-			    title: title,
-			    type: 'alert',
-			    text: text
-			});
-		}
-		
+		let promise = new Promise((resolve, reject) => {
+			if (modal) {
+				dhtmlx.message({
+					title: title,
+					type: 'alert',
+					text: text,
+					callback: function() {
+						resolve();
+					}
+				});
+			} else {
+				dhtmlx.message({
+					title: title,
+					text: text
+				});
+				resolve();
+			}
+		});
+        return promise;
 	}
 
 	static warning (title, text, modal = false) {
-		if (modal) {
-			dhtmlx.message({
-			    title: title,
-			    type: 'alert-warning',
-			    text: text
-			});
-		} else {
-			dhtmlx.alert({
-			    title: title,
-			    type: 'alert-warning',
-			    text: text
-			});
-		}
+		let promise = new Promise((resolve, reject) => {
+			if (modal) {
+				dhtmlx.message({
+					title: title,
+					type: 'alert-warning',
+					text: text,
+					callback: function() {
+						resolve();
+					}
+				});
+			} else {
+				dhtmlx.message({
+					title: title,
+					text: text
+				});
+				resolve();
+			}
+		});
+        return promise;
 	}
 
 	static error (title, text, modal = false) {
-		if (modal) {
-			dhtmlx.message({
-			    title: title,
-			    type: 'alert-error',
-			    text: text
-			});
-		} else {
-			dhtmlx.alert({
-			    title: title,
-			    type: 'alert-error',
-			    text: text
-			});
-		}
+		let promise = new Promise((resolve, reject) => {
+			if (modal) {
+				dhtmlx.message({
+					title: title,
+					type: 'alert-error',
+					text: text,
+					callback: function() {
+						resolve();
+					}
+				});
+			} else {
+				dhtmlx.message({
+					title: title,
+					type: 'error',
+					text: text
+				});
+				resolve();
+			}
+		});
+        return promise;
 	}
 
 	static confirm (title, text, ok, cancel) {
-            let promise = new Promise((resolve, reject) => {
-                dhtmlx.confirm({
-                    title: title,
-                    text: text,
-                    ok: ok,
-                    cancel: cancel,
-                    callback: function(response) {
-                        if (response) {
-                            resolve();
-                        } else {
-                            reject();    
-                        }
-                    }
-                });
-            });
-            return promise;
+		let promise = new Promise((resolve, reject) => {
+			dhtmlx.confirm({
+				title: title,
+				text: text,
+				ok: ok,
+				cancel: cancel,
+				callback: function(response) {
+					if (response) {
+						resolve();
+					} else {
+						reject();    
+					}
+				}
+			});
+		});
+		return promise;
 	}
 }
